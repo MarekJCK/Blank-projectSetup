3ec687f4e521b374565f1ac7c4987180369f49f1
MarekJCK <jackomarek59@gmail.com>
2017-04-14 15:04:34 +0200

    js folder with application.js file added html og meta data added and gitignore update

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,46 @@
-var gulp 		    = require('gulp');
+var gulp        = require('gulp');
 var sass        = require('gulp-sass');
-var minifyCSS 	= require('gulp-minify-css');
-var webserver 	= require('gulp-webserver');
+var minifyCSS   = require('gulp-minify-css');
+var webserver   = require('gulp-webserver');
 var watch       = require('gulp-watch');
+var uglify      = require('gulp-uglify');
+var pump        = require('pump');
 
 gulp.task('sass', function() {
-  gulp.src('./src/*.scss')
-    .pipe(sass({
-      css: 'style',
-      sass: 'src'
-    }))
-    .on('error', function(error) { console.log(error) })
-    .pipe(minifyCSS())
-    .pipe(gulp.dest('style'));
+    gulp.src('./src/*.scss')
+        .pipe(sass({
+            css: 'style',
+            sass: 'src'
+        }))
+        .on('error', function(error) { console.log(error) })
+        .pipe(minifyCSS())
+        .pipe(gulp.dest('style'));
 });
 
 gulp.task('webserver', function() {
-  gulp.src('./')
-    .pipe(webserver({
-      livereload: true,
-      directoryListing: true,
-      open: true
-    }));
+    gulp.src('./')
+        .pipe(webserver({
+            livereload: true,
+            directoryListing: true,
+            open: true
+        }));
 });
 
 gulp.task('watch', function() {
-  gulp.watch(['src/**/*.scss'], ['sass']);
+    gulp.watch(['src/**/*.scss'], ['sass']);
+    gulp.watch(['js/**/*.js'], ['compress']);
 });
 
-gulp.task('default', ['watch', 'webserver']);
+gulp.task('compress', function (cb) {
+    pump([
+            gulp.src('js/application.js'),
+            uglify(),
+            gulp.dest('dist')
+        ],
+        cb
+    );
+});
+
+gulp.task('default', ['watch', 'webserver', 'compress'], function() {
+    console.log('Up and running, enjoy!')
+});
